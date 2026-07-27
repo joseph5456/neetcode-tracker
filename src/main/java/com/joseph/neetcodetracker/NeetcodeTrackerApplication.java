f4a6172a48a459a96cfad961924addeccf371015
Joseph Nguyen <tracy201156@gmail.com>
2026-07-27 15:46:33 -0400

    Add application entry point

---
+++ src/main/java/com/joseph/neetcodetracker/NeetcodeTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.joseph.neetcodetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeetcodeTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NeetcodeTrackerApplication.class, args);
+    }
+}
